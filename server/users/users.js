25fd1b7cc0525fa5b2047481f8a276836e1f4046
Rosendo <rosendomarquez30@gmail.com>
2024-05-24 21:49:44 -0700

    Modern looking Login page with local storage

---
+++ server/users/users.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+//image schema to house url with comments
+const imageSchema = new mongoose.Schema(
+{
+    url: { type: String, required: true },
+    comments: [{ type: String }] 
+});
+
+const userSchema = new mongoose.Schema(
+{
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, unique: true },
+    images: [imageSchema] //array of images
+});
+
+// Define User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
